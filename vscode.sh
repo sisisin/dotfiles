ff29159a0b5e346b3c83b9e5495d61f161b11c73
sisisin <azsisisin@gmail.com>
2016-04-15 18:05:20 +0900

    add

---
+++ vscode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+VSC_APP_DIR="${HOME}/Library/Application Support/Code/User"
+VSC_FILES_DIR="${HOME}/dotfiles/vscode"
+cd ${VSC_FILES_DIR}
+
+find . -type f | while read f
+do
+  [[ ${f} = ".DS_Store" ]] && continue
+  cp -f "${VSC_FILES_DIR}/${f}" "${VSC_APP_DIR}/${f}"
+done
+echo $(tput setaf 2)Deploy VSCode setting files complete!. ✔︎$(tput sgr0)
