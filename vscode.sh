a230f08a6970e6eaa63ddf3d6dd769c5d9ddb6a4
sisisin <azsisisin@gmail.com>
2017-01-28 02:58:13 +0900

    fix(vscode) use symlink

--- vscode.sh
+++ vscode.sh
@@ -6,6 +6,6 @@ cd ${VSC_FILES_DIR}
 find . -type f | while read f
 do
   [[ ${f} = ".DS_Store" ]] && continue
-  cp -f "${VSC_FILES_DIR}/${f}" "${VSC_APP_DIR}/${f}"
+  ln -snfv "${VSC_FILES_DIR}/${f}" "${VSC_APP_DIR}/${f}"
 done
 echo $(tput setaf 2)Deploy VSCode setting files complete!. ✔︎$(tput sgr0)
