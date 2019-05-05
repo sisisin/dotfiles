46d96e2f7933a1570f31f82dd6987fa21b254818
sisisin <azsisisin@gmail.com>
2019-05-06 00:50:29 +0900

    remove extensions

--- scripts/check_diff_vscode_extension.rb
+++ scripts/check_diff_vscode_extension.rb
@@ -2,7 +2,7 @@ require_relative "vscode_extension_checker"
 
 msg = <<-EOS
 vscode's extension has diff from dotfiles.
-you should run `deploy.sh`
+you should run `deploy.sh` or sync commands
 EOS
 
-print(msg) if (VscodeExtensionChecker.new).should_update?
+print(msg) if VscodeExtensionChecker.new.should_update?
