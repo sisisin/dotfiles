171fe5ef433e40051695e35895302c61649c8644
sisisin <azsisisin@gmail.com>
2019-11-24 07:24:32 +0900

    fix

--- vscode.sh
+++ vscode.sh
@@ -16,6 +16,6 @@ function create_symlink() {
     ln -snfv "${VSC_FILES_DIRECTORY}/snippets" "${VSC_APP_DIR}"
 }
 
-$DOT_DIRECTORY/bin/vsc-ext-manager install-and-append-to-file
+"$DOT_DIRECTORY/bin/vsc-ext-manager" install-and-append-to-file
 create_symlink
 echo $(tput setaf 2)Deploy VSCode setting files complete!. ✔︎$(tput sgr0)
